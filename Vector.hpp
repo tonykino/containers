197c6d63133fa56d06eb791d6ced76e3b7905c2a
tonykino <tony.kino.3@gmail.com>
2022-05-14 18:59:27 +0200

    Vector first draft

---
+++ Vector.hpp
@@ -0,0 +1,167 @@
+#ifndef VECTOR_TPP
+#define VECTOR_TPP
+
+#include <memory>
+#include <iostream>
+#include <algorithm>
+
+namespace ft {
+	
+template <class T, class Allocator = std::allocator<T> >
+class vector {
+public:
+	// types:
+	typedef T value_type;
+	typedef Allocator allocator_type;
+	typedef typename Allocator::reference reference;
+	typedef typename Allocator::const_reference const_reference;
+	typedef typename Allocator::pointer pointer;
+	typedef typename Allocator::const_pointer const_pointer;
+
+	// typedef implementation defined iterator; // See 23.1  // TODO
+	// typedef implementation defined const_iterator; // See 23.1 // TODO
+	// typedef std::reverse_iterator<iterator> reverse_iterator; // TODO
+	// typedef std::reverse_iterator<const_iterator> const_reverse_iterator; // TODO
+	
+	typedef std::ptrdiff_t difference_type;
+	typedef std::size_t size_type;
+
+	// 23.2.4.1 construct/copy/destroy:	
+	explicit vector(const allocator_type& alloc = allocator_type())
+	: _alloc(alloc), _start(_alloc.allocate(0)), _end(_start), _end_capa(_end) {}
+
+	// explicit vector(size_type n, const T& value = T(), const allocator_type& alloc = allocator_type())
+	// : _alloc(alloc), _start(_alloc.allocate(n)), _end(_start + n), _end_capa(_end) {
+
+	// }
+
+	// template <class InputIterator>
+	// vector(InputIterator first, InputIterator last, const allocator_type& = allocator_type());
+
+	// vector(const vector<T,allocator_type>& x);
+	// ~vector();
+	
+	// vector<T,allocator_type>& operator=(const vector<T,allocator_type>& x);
+
+	// template <class InputIterator>
+	// void assign(InputIterator first, InputIterator last);
+
+	// void assign(size_type n, const T& u);
+	// allocator_type get_allocator() const;
+
+	// // iterators:
+	// iterator begin();
+	// const_iterator begin() const;
+	// iterator end();
+	// const_iterator end() const;
+	// reverse_iterator rbegin();
+	// const_reverse_iterator rbegin() const;
+	// reverse_iterator rend();
+	// const_reverse_iterator rend() const;
+
+	// // 23.2.4.2 capacity:
+	size_type size() const {
+		return _end - _start;
+	}
+
+	size_type max_size() const {
+		return _alloc.max_size();
+	}
+
+	// void resize(size_type sz, T c = T());
+	
+	size_type capacity() const {
+		return _end_capa - _start;
+	}
+
+	// bool empty() const;
+	void reserve(size_type n) {
+		// TODO : handle n > max_size()
+		if (n <= capacity()) return ;
+
+		size_type old_size = size();
+		
+		// allocate new space
+		pointer new_start = _alloc.allocate(n);
+
+		// copy elts to new reserved space
+		for (size_type i = 0; i < old_size; i++)
+			_alloc.construct(new_start + i, _start[i]);
+
+		// destroy old elts
+		for (size_type i = 0; i < old_size; i++)
+			_alloc.destroy(_start + i);
+
+		// deallocate old space
+		_alloc.deallocate(_start, capacity());
+
+		// update pointers
+		_start = new_start;
+		_end = _start + old_size;
+		_end_capa = _start + n;
+	}
+
+	// // element access:
+	// reference operator[](size_type n);
+	// const_reference operator[](size_type n) const;
+	// const_reference at(size_type n) const;
+	// reference at(size_type n);
+	// reference front();
+	// const_reference front() const;
+	// reference back();
+	// const_reference back() const;
+
+	// // 23.2.4.3 modifiers:
+	void push_back(const T& v) {
+		// Increase capacity if needed
+		if (size() >= capacity())
+			reserve(std::max<size_type>(1, 2 * capacity()));
+
+		// construct new elt
+		_alloc.construct(_end, v);
+		_end++;
+	}
+	// void pop_back();
+	// iterator insert(iterator position, const T& x);
+	// void insert(iterator position, size_type n, const T& x);
+
+	// template <class InputIterator>
+	// void insert(iterator position, InputIterator first, InputIterator last);
+	
+	// iterator erase(iterator position);
+	// iterator erase(iterator first, iterator last);
+	// void swap(vector<T,Allocator>&);
+	// void clear();
+
+private:
+	allocator_type	_alloc;
+	pointer			_start;
+	pointer			_end;
+	pointer			_end_capa;
+};
+
+template <class T, class Allocator>
+bool operator==(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+template <class T, class Allocator>
+bool operator< (const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+template <class T, class Allocator>
+bool operator!=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+template <class T, class Allocator>
+bool operator> (const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+template <class T, class Allocator>
+bool operator>=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+template <class T, class Allocator>
+bool operator<=(const vector<T,Allocator>& x, const vector<T,Allocator>& y);
+
+// specialized algorithms:
+template <class T, class Allocator>
+void swap(vector<T,Allocator>& x, vector<T,Allocator>& y);
+
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,96 +1,162 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include "Stack.tpp"
+#include "Stack.hpp"
+#include "Vector.hpp"
 
 
 
 int main(void)
 {
 
-
-    /*{
-        // TEST VECTOR
-        std::vector<int> data;
-        data.push_back(1);
-        data.push_back(2);
-        data.push_back(3);
-
-        for (std::vector<int>::iterator it = data.begin(); it < data.end(); it++)
-            std::cout << *it << std::endl;
-    }*/
-
     {
-        // TEST STD::STACK
-        std::cout << "   --- STD::STACK --- " << std::endl;
-        std::stack<int> c1;
-        for (int i = 0; i < 5; i++) {
-            c1.push((i+1)*2);
-            std::cout << "size: " << c1.size() << " "
-                      << "top: "  << c1.top()  << std::endl;
-        }
+        // TEST STD::VECTOR
+		std::cout << std::endl << "   --- STD::VECTOR --- " << std::endl;
 
-		std::stack<int> c2(c1);
+        std::vector<int> v1;
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(1);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(2);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(3);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(4);
+        v1.push_back(5);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(6);
+        v1.push_back(7);
+        v1.push_back(8);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(9);
+        v1.push_back(10);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
 
-		std::cout << "Comparaison tests:" << std::endl;
-		std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
-		std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
-		std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
-		std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
-		std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
-		std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+        // std::vector<int> v2(v1);
+		// std::cout << "v2: size:" << v2.size() << " capacity:" << v2.capacity() << " max_size:" << v2.max_size() << std::endl;
+        // v2.push_back(11);
+		// std::cout << "v2: size:" << v2.size() << " capacity:" << v2.capacity() << " max_size:" << v2.max_size() << std::endl;
 
-		std::cout << "C1: " << std::endl;
-        for (int i = 0; i < 5; i++) {
-            std::cout << "size: " << c1.size() << " "
-                      << "top: "  << c1.top()  << std::endl;
-            c1.pop();
-        }
 
-		std::cout << "C2: " << std::endl;
-		for (int i = 0; i < 5; i++) {
-            std::cout << "size: " << c2.size() << " "
-                      << "top: "  << c2.top()  << std::endl;
-            c2.pop();
-        }
 
+        // std::vector<int> v3(10, 3);
+		// std::cout << "v3: size:" << v3.size() << " capacity:" << v3.capacity() << " max_size:" << v3.max_size() << std::endl;
+
+
+        // for (std::vector<int>::iterator it = v1.begin(); it < v1.end(); it++)
+        //     std::cout << *it << std::endl;
     }
 
 
     {
-        // TEST FT::STACK
-        std::cout << "   --- FT::STACK --- " << std::endl;
-        ft::stack<int> c1;
-        for (int i = 0; i < 5; i++) {
-            c1.push((i+1)*2);
-            std::cout << "size: " << c1.size() << " "
-                      << "top: "  << c1.top()  << std::endl;
-        }
+        // TEST FT::VECTOR
+		std::cout << std::endl << "   --- FT::VECTOR --- " << std::endl;
 
-		ft::stack<int> c2(c1);
+        ft::vector<int> v1;
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(1);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(2);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(3);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(4);
+        v1.push_back(5);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(6);
+        v1.push_back(7);
+        v1.push_back(8);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
+        v1.push_back(9);
+        v1.push_back(10);
+		std::cout << "v1: size:" << v1.size() << " capacity:" << v1.capacity() << " max_size:" << v1.max_size() << std::endl;
 
-		std::cout << "Comparaison tests:" << std::endl;
-		std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
-		std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
-		std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
-		std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
-		std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
-		std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+        // std::vector<int> v2(v1);
+		// std::cout << "v2: size:" << v2.size() << " capacity:" << v2.capacity() << " max_size:" << v2.max_size() << std::endl;
+        // v2.push_back(11);
+		// std::cout << "v2: size:" << v2.size() << " capacity:" << v2.capacity() << " max_size:" << v2.max_size() << std::endl;
 
-		std::cout << "C1: " << std::endl;
-        for (int i = 0; i < 5; i++) {
-            std::cout << "size: " << c1.size() << " "
-                      << "top: "  << c1.top()  << std::endl;
-            c1.pop();
-        }
 
-		std::cout << "C2: " << std::endl;
-		for (int i = 0; i < 5; i++) {
-            std::cout << "size: " << c2.size() << " "
-                      << "top: "  << c2.top()  << std::endl;
-            c2.pop();
-        }
+
+        // std::vector<int> v3(10, 3);
+		// std::cout << "v3: size:" << v3.size() << " capacity:" << v3.capacity() << " max_size:" << v3.max_size() << std::endl;
+
+
+        // for (std::vector<int>::iterator it = v1.begin(); it < v1.end(); it++)
+        //     std::cout << *it << std::endl;
     }
+
+    // {
+    //     // TEST STD::STACK
+    //     std::cout << "   --- STD::STACK --- " << std::endl;
+    //     std::stack<int> c1;
+    //     for (int i = 0; i < 5; i++) {
+    //         c1.push((i+1)*2);
+    //         std::cout << "size: " << c1.size() << " "
+    //                   << "top: "  << c1.top()  << std::endl;
+    //     }
+
+	// 	std::stack<int> c2(c1);
+
+	// 	std::cout << "Comparaison tests:" << std::endl;
+	// 	std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
+	// 	std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
+	// 	std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
+	// 	std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
+	// 	std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
+	// 	std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+
+	// 	std::cout << "C1: " << std::endl;
+    //     for (int i = 0; i < 5; i++) {
+    //         std::cout << "size: " << c1.size() << " "
+    //                   << "top: "  << c1.top()  << std::endl;
+    //         c1.pop();
+    //     }
+
+	// 	std::cout << "C2: " << std::endl;
+	// 	for (int i = 0; i < 5; i++) {
+    //         std::cout << "size: " << c2.size() << " "
+    //                   << "top: "  << c2.top()  << std::endl;
+    //         c2.pop();
+    //     }
+
+    // }
+
+
+    // {
+    //     // TEST FT::STACK
+    //     std::cout << "   --- FT::STACK --- " << std::endl;
+    //     ft::stack<int> c1;
+    //     for (int i = 0; i < 5; i++) {
+    //         c1.push((i+1)*2);
+    //         std::cout << "size: " << c1.size() << " "
+    //                   << "top: "  << c1.top()  << std::endl;
+    //     }
+
+	// 	ft::stack<int> c2(c1);
+
+	// 	std::cout << "Comparaison tests:" << std::endl;
+	// 	std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
+	// 	std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
+	// 	std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
+	// 	std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
+	// 	std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
+	// 	std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+
+	// 	std::cout << "C1: " << std::endl;
+    //     for (int i = 0; i < 5; i++) {
+    //         std::cout << "size: " << c1.size() << " "
+    //                   << "top: "  << c1.top()  << std::endl;
+    //         c1.pop();
+    //     }
+
+	// 	std::cout << "C2: " << std::endl;
+	// 	for (int i = 0; i < 5; i++) {
+    //         std::cout << "size: " << c2.size() << " "
+    //                   << "top: "  << c2.top()  << std::endl;
+    //         c2.pop();
+    //     }
+    // }
 
 	return 0;
 }
