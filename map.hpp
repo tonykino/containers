d9a8e088e5ee59ccd416134937ea00cbce215382
Tony KINO <tony.kino.3@gmail.com>
2022-08-17 20:26:56 +0200

    Add compare_value

--- map.hpp
+++ map.hpp
@@ -33,18 +33,18 @@ public:
 	typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
 	typedef std::size_t size_type;
 
-	// class value_compare: public binary_function<value_type,value_type,bool> { // todo : what is this ?!
-	//     friend class map;
+	class value_compare: public binary_function<value_type, value_type, bool> {
+	    friend class map;
 		
-	//     protected:
-	//     Compare comp;
-	//     value_compare(Compare c): comp(c) {}
+	    protected:
+	    Compare comp;
+	    value_compare(Compare c): comp(c) {}
 
-	//     public:
-	//     bool operator()(const value_type& x, const value_type& y) const {
-	//         return comp(x.first, y.first);
-	//     }
-	// };
+	    public:
+	    bool operator()(const value_type& x, const value_type& y) const {
+	        return comp(x.first, y.first);
+	    }
+	};
 
 	// 23.3.1.1 construct/copy/destroy:
 	explicit map(const Compare& comp = Compare(), const Allocator& = Allocator())
@@ -96,7 +96,9 @@ public:
 	// T& operator[](const key_type& x);
 
 	// // modifiers:
-	// pair<iterator, bool> insert(const value_type& x);
+	std::pair<iterator, bool> insert(const value_type& x) {
+
+	}
 	// iterator insert(iterator position, const value_type& x);
 
 	// template <class InputIterator>
@@ -113,7 +115,7 @@ public:
 	// value_compare value_comp() const;
 
 	// // 23.3.1.3 map operations:
-	// iterator find(const key_type& x);
+	iterator find(const key_type& x);
 	// const_iterator find(const key_type& x) const;
 	// size_type count(const key_type& x) const;
 	// iterator lower_bound(const key_type& x);
