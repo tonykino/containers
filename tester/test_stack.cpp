e15e4a80b039fbd6c9ea3381c92be04ec0ebbbc5
Tony KINO <tony.kino.3@gmail.com>
2022-08-31 14:54:02 +0200

    Add some tests on stack

--- tester/test_stack.cpp
+++ tester/test_stack.cpp
@@ -1,4 +1,6 @@
 #include "test.hpp"
+#include <deque>
+#include <list>
 
 #ifdef STD
 	#include <stack>
@@ -8,35 +10,103 @@
 
 void test_stack(void)
 {
-	NS::stack<int> c1;
-	for (int i = 0; i < 5; i++) {
-		c1.push((i+1)*2);
-		std::cout << "size: " << c1.size() << " "
-					<< "top: "  << c1.top()  << std::endl;
+	{
+		NS::stack<int> c1;
+		for (int i = 0; i < 5; i++) {
+			c1.push((i+1)*2);
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+		}
+
+		NS::stack<int> c2(c1);
+
+		std::cout << "Comparaison tests:" << std::endl;
+		std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
+		std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
+		std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
+		std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
+		std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
+		std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+
+		std::cout << "C1: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+			c1.pop();
+		}
+
+		std::cout << "C2: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c2.size() << " "
+						<< "top: "  << c2.top()  << std::endl;
+			c2.pop();
+		}
 	}
 
-	NS::stack<int> c2(c1);
+	{
+		NS::stack<int, std::deque<int> > c1;
+		for (int i = 0; i < 5; i++) {
+			c1.push((i+1)*2);
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+		}
 
-	std::cout << "Comparaison tests:" << std::endl;
-	std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
-	std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
-	std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
-	std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
-	std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
-	std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+		NS::stack<int, std::deque<int> > c2(c1);
 
-	std::cout << "C1: " << std::endl;
-	for (int i = 0; i < 5; i++) {
-		std::cout << "size: " << c1.size() << " "
-					<< "top: "  << c1.top()  << std::endl;
-		c1.pop();
+		std::cout << "Comparaison tests:" << std::endl;
+		std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
+		std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
+		std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
+		std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
+		std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
+		std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+
+		std::cout << "C1: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+			c1.pop();
+		}
+
+		std::cout << "C2: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c2.size() << " "
+						<< "top: "  << c2.top()  << std::endl;
+			c2.pop();
+		}
 	}
 
-	std::cout << "C2: " << std::endl;
-	for (int i = 0; i < 5; i++) {
-		std::cout << "size: " << c2.size() << " "
-					<< "top: "  << c2.top()  << std::endl;
-		c2.pop();
+	{
+		NS::stack<int, std::list<int> > c1;
+		for (int i = 0; i < 5; i++) {
+			c1.push((i+1)*2);
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+		}
+
+		NS::stack<int, std::list<int> > c2(c1);
+
+		std::cout << "Comparaison tests:" << std::endl;
+		std::cout << "c1 == c2 " << (c1 == c2) << std::endl;
+		std::cout << "c1 != c2 " << (c1 != c2) << std::endl;
+		std::cout << "c1 > c2 " << (c1 > c2) << std::endl;
+		std::cout << "c1 >= c2 " << (c1 >= c2) << std::endl;
+		std::cout << "c1 < c2 " << (c1 < c2) << std::endl;
+		std::cout << "c1 <= c2 " << (c1 <= c2) << std::endl;
+
+		std::cout << "C1: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c1.size() << " "
+						<< "top: "  << c1.top()  << std::endl;
+			c1.pop();
+		}
+
+		std::cout << "C2: " << std::endl;
+		for (int i = 0; i < 5; i++) {
+			std::cout << "size: " << c2.size() << " "
+						<< "top: "  << c2.top()  << std::endl;
+			c2.pop();
+		}
 	}
 
 }
